c043a28456a112ded2a7b41c7bf0e25759fb7083
Abigail <abigail@picnic-media.com>
2021-01-05 10:58:43 +0000

    adding cname

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,18 +5,21 @@ const Contact = () => {
   return (
     <>
       <Fade>
-        <section className="hero is-white is-fullheight">
-          <div className="hero-body is-justify-content-space-around">
-            <section className="section">
+        <section className="hero is-white is-large">
+          <div className="hero-body is-justify-content-space-around is-flex is-flex-wrap-wrap contact-wrapper">
+            <section className="section inquiries-section" style={{ width: "50%" }}>
               <div className="container">
                 <h1 className="title is-1" style={{ wordBreak: "normal" }}>
                   INQUIRIES
                 </h1>
               </div>
             </section>
-            <section className="section">
+            <section
+              className="section is-align-self-center inquiries-info-section"
+              style={{ width: "50%" }}
+            >
               <div className="container">
-                <h1 className="subtitle is-4">
+                <h1 className="subtitle is-4 inquiries-info">
                   For all inquiries, please contact Paul at {""}
                   <a
                     href="mailto:outlaw718@btinternet.com"
